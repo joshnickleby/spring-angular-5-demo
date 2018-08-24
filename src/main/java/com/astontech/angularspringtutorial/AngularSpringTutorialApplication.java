12f974d10f25f260a1d2bc38e3c93d70b4201d78
exmarxdespot <ofgodsandmythos@gmail.com>
2018-08-24 13:50:24 -0500

    Intialize repo

---
+++ src/main/java/com/astontech/angularspringtutorial/AngularSpringTutorialApplication.java
@@ -0,0 +1,12 @@
+package com.astontech.angularspringtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularSpringTutorialApplication.class, args);
+    }
+}
